f6abf8c7a2360195e70a3b7c75b0503e3b75b1f1
VM window <mostlyharmlessgit@yahoo.com>
2026-07-28 11:11:16 -0700

    modified logC and kernunos to put logfiles in users root directory working on assistant to find documentation

--- src/c/logc.c
+++ src/c/logc.c
@@ -5,14 +5,43 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
 
 bool LogCreated = false;
 void LogC(const char *Message) { FILE *file;
-    if (!LogCreated) { file = fopen(LOGFILE, "w");
+    char *path;
+#ifdef _WIN32
+   path = getenv("USERPROFILE");
+#else
+    path = getenv("USER");
+#endif
+    if ( path == NULL )
+    {
+        perror("getenv() error"); return ;
+    }
+    size_t lenp = strlen(path);
+    size_t len = strlen(LOGFILE);
+    char pathandfile[len+lenp+2];
+#ifdef _WIN32
+    snprintf(pathandfile, sizeof(pathandfile), "%s%s%s", path, "\\", LOGFILE);
+#else
+    snprintf(pathandfile, sizeof(pathandfile), "%s%s%s", path,"/",LOGFILE);
+#endif
+    if (!LogCreated) { file = fopen(pathandfile, "w");
         LogCreated = true; }
-    else file = fopen(LOGFILE, "a");
+    else file = fopen(pathandfile, "a");
     if (file == NULL) { if (LogCreated) LogCreated = false; return ; }
     else {
+        time_t timeStamp;
+        time(&timeStamp);
+        fprintf(file, "%s", ctime(&timeStamp));
+        fprintf(file, "%s", " ");
         fprintf(file, "%s", Message);
         fprintf(file,"\n");
         fclose(file);
