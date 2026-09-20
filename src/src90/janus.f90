f8111fb62aa8afd9729642907ac0b149e1f29594
mostlyharmless <mostlyharmlessgit@yahoo.com>
2026-09-19 17:10:46 -0700

    another gnuplot tweak

--- src/src90/janus.f90
+++ src/src90/janus.f90
@@ -2872,7 +2872,8 @@ open(unitno1, file = "/tmp/zernike.tmp", action="write", iostat=ierr)
  write(unitno1,*) "unset key";
  write(unitno1,*) "myBoxWidth = 0.8";
  write(unitno1,*) "set offsets 0,0,0.5-myBoxWidth/2.,0.5";
- write(unitno1,*) "plot $Data using (0.5*$2):0:(0.5*$2):(myBoxWidth/2.):($3):ytic(1) with boxxy lc rgb var";
+! write(unitno1,*) "plot $Data using (0.5*$2):0:(0.5*$2):(myBoxWidth/2.):($3):ytic(1) with boxxy lc rgb var";
+ write(unitno1,*) "plot $Data using (0):($0):(0):($2):($0-myBoxWidth/2.):($0+myBoxWidth/2.):($3):ytic(1) with boxxy lc rgb var"
  close(unitno1)
 ! this line clears the counter, no longer does anything with gp or the filename
  call Ccounter(100)
